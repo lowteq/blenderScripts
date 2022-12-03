732d741e94eb0b2af0456fe8a3e15d32e622f26a
lowteq <qukumu@gmail.com>
2022-12-03 17:47:19 +0900

    add license

--- scripts/adjustedges.py
+++ scripts/adjustedges.py
@@ -1,3 +1,7 @@
+#Copyright (c) 2022 lowteq
+#Released under the MIT license
+#https://opensource.org/licenses/mit-license.php
+
 bl_info = {
     "name": "Adjust edge length",
     "author": "lowteq",
